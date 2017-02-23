37fa4be4db97bc80ac31a094f0fa3f7cc7cd4246
kolszer <k.olszer@gmail.com>
2017-02-23 15:16:42 +0100

    Add implementation of OutOfMem Project

--- OutOfMemProject/src/main/java/App.java
+++ OutOfMemProject/src/main/java/App.java
@@ -1,5 +1,21 @@
-public class App{
-	public static void main(String[] args) {
-		System.out.println("SDFSF");
-	}
+import java.util.ArrayList;
+import java.util.List;
+
+//Aplikacja konczy sie wyjatkiem OutOfMemory
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+    		//Tworzymy liste intow
+		List<Integer> intList = new ArrayList<Integer>();
+
+		//Nieskonczona petla
+		for(;;)
+		{
+			//Dodajemy do listy liczbe ((2^32)/2)-1, w pewnym momencie
+			//lista jest tak duza, ze aplikacja konczy sie wyjatkiem OutOfMemory
+			intList.add(Integer.MAX_VALUE);
+		}
+    }
 }
